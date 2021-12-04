8cf53519c48cdd5ffaa7df319326b5f8a1ffe8e2
单志林 <940259702@qq.com>
2021-12-04 20:40:23 +0800

    doc:添加 icon 文档

---
+++ src/components/Icon/icon.stories.tsx
@@ -0,0 +1,75 @@
+/*
+ * @Author: shanzhilin
+ * @Date: 2021-12-04 15:24:16
+ * @LastEditors: shanzhilin
+ * @LastEditTime: 2021-12-04 16:35:30
+ */
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { faAdjust,faCoffee } from "@fortawesome/free-solid-svg-icons";
+import Icon, { IconProps } from "./icon";
+
+export default {
+  title: "Icon",
+  component: Icon,
+  argTypes: {
+    size: {
+      description: "icon的不同尺寸",
+    },
+    style: {
+      description: "自定义样式",
+    },
+  },
+} as ComponentMeta<typeof Icon>;
+
+// 单个组件模板
+const Template: ComponentStory<typeof Icon> = (args) => <Icon {...args} />;
+
+// 默认Icon
+export const DefaultIcon = Template.bind({});
+DefaultIcon.args = {
+  icon: faAdjust,
+  style: {
+    fontSize: "50px",
+  },
+};
+
+// 多个组件渲染模板
+const ListTemplate: ComponentStory<typeof Icon> = (args) => {
+  const { children } = args;
+  const items = children as Array<IconProps>;
+  return (
+    <>
+      {items.map((item: IconProps) => (
+        <Icon {...item} />
+      ))}
+    </>
+  );
+};
+
+//  不同样式的组件
+export const DiffTypeIcon = ListTemplate.bind({});
+DiffTypeIcon.args = {
+  children: [
+    {
+        icon:faCoffee,
+        theme: 'success',
+        style: {
+            fontSize:'50px'
+        }
+    },
+    {
+        icon:faCoffee,
+        theme: 'warning',
+        style: {
+            fontSize:'50px'
+        }
+    },
+    {
+        icon:faCoffee,
+        theme: 'danger',
+        style: {
+            fontSize:'50px'
+        }
+    }
+  ],
+};
--- src/components/Icon/icon.tsx
+++ src/components/Icon/icon.tsx
@@ -2,7 +2,7 @@
  * @Author: shanzhilin
  * @Date: 2021-11-26 23:49:22
  * @LastEditors: shanzhilin
- * @LastEditTime: 2021-11-26 23:57:45
+ * @LastEditTime: 2021-12-04 16:30:17
  */
 import React from "react";
 import classNames from "classnames";
@@ -11,10 +11,26 @@ import { FontAwesomeIcon,FontAwesomeIconProps } from "@fortawesome/react-fontawe
 export type ThemeProps = 'primary' | 'secondary' | 'success' | 'info' | 'warning' | 'danger' | 'dark' | 'light'
 
 export interface IconProps extends FontAwesomeIconProps {
+    /** icon 样式*/ 
     theme?: ThemeProps
 }
 
-const Icon:React.FC<IconProps>=(props) => {
+/**
+ *> ## **引用方式**
+ * **import Icon from 'labelDesign'**
+ * >
+ * > ## **图标库**
+ * **这里引用 FontaweSome 组件库
+ * [FontaweSome](https://fontawesome.com/v5.15/how-to-use/on-the-web/using-with/react)
+ * >
+ * > ## **基本用法**
+```js
+    import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+<FontAwesomeIcon icon={faCoffee} />
+```    
+*/
+export const Icon:React.FC<IconProps>=(props) => {
     const {className,theme,...restProps} = props
     const classes = classNames('icon',className,{
         [`icon-${theme}`]: theme
